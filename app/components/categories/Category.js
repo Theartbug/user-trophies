a7f196e325f4c43ba1f41f89beed33f7949286e0
G.Provost <grace.g.provost@gmail.com>
2018-05-19 10:05:02 -0700

    pressing see all button hides and shows trophies over 5

--- app/components/categories/Category.js
+++ app/components/categories/Category.js
@@ -12,23 +12,33 @@ export default class Categories extends PureComponent {
   changeVisible = () => {
     const { open } = this.state;
 
-    this.setState({ open: !open});
-  }
+    this.setState({ open: !open });
+  };
 
   render() {
 
     const { name, trophies } = this.props.category;
     const { changeVisible } = this;
     const { open } = this.state;
+    //create a new array and shorten it
+    const shortenedTrophies = trophies.slice();
+    shortenedTrophies.length = 5;
+
+    const visibleTrophies = open ? trophies : shortenedTrophies;
 
     return (
       <li className="category">
         <div className="header">
           <h1>{name}</h1>
-          <button type="button" onClick={changeVisible}>See All <Chevron className={`${open === true ? 'active' : ''}`}/></button>
+          <button type="button" onClick={changeVisible}>See All <Chevron className={`${open ? 'active' : ''}`}/></button>
         </div>
         <ul>
-          {trophies.map((trophy, i) => <Trophy key={i} trophy={trophy}/>)}
+          {open ?
+            visibleTrophies.map((trophy, i) => <Trophy key={i} trophy={trophy}/>)
+            :
+            visibleTrophies.map((trophy, i) => <Trophy key={i} trophy={trophy}/>)
+          }
+
         </ul>
       </li>
     );
